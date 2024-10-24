594005c50e516ea40e53a5eee9ac1f4a3e900f9e
jainam3622 <clash.royale20001921@gmail.com>
2024-10-25 00:17:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7763566676:AAFPUFSFHkdnPppDy5ov6tumAsWuTcgltUU")
+BOT_TOKEN=("7507854182:AAGxXjIQY7vzi3Crj5t3bJzH83zr0CYBruI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("@God_x_Sikandar")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1329951770")
+ADMIN_IDS=("1216702307")
 
 
 
